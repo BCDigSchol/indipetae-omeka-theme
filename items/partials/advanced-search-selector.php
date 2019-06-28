05f296bd869853dd1a8f2e38c842cfb1f77e43bf
Anna Kijas <annakijas1@gmail.com>
2019-06-28 12:03:11 -0400

    updated dropdown options

--- items/partials/advanced-search-selector.php
+++ items/partials/advanced-search-selector.php
@@ -8,7 +8,7 @@
                 <li class="adv-select__opt adv-select__opt--sender" data-field="sender">Sender</li>
                 <li class="adv-select__opt adv-select__opt--recipient" data-field="recipient">Recipient</li>
                 <li class="adv-select__opt adv-select__opt--model" data-field="jmodel">Models/Saints/Missionaries</li>
-                <li class="adv-select__opt adv-select__opt--other_names" data-field="other_name">Other persons mentioned</li>
+                <li class="adv-select__opt adv-select__opt--other_names" data-field="other_name">Other names</li>
             </ul>
         </div>
     </div>
@@ -21,9 +21,9 @@
         </button>
         <div class="dropdown-menu adv-select__opts">
             <ul>
-                <li class="adv-select__opt adv-select__opt--from" data-field="from">From</li>
+                <li class="adv-select__opt adv-select__opt--from" data-field="from">From (City)</li>
                 <li class="adv-select__opt adv-select__opt--to" data-field="to">To</li>
-                <li class="adv-select__opt adv-select__opt--destinations" data-field="destinations">Destination</li>
+                <li class="adv-select__opt adv-select__opt--destinations" data-field="destinations">Destination(s)</li>
             </ul>
         </div>
     </div>
@@ -51,7 +51,7 @@
             <ul>
                 <li class="adv-select__opt adv-select__opt--grade" data-field="grade">Grade S.J.</li>
                 <li class="adv-select__opt adv-select__opt--anterior_desire" data-field="anterior_desire">Anterior Desire</li>
-                <li class="adv-select__opt adv-select__opt--left_for_mission" data-field="left_for_mission">Left for Mission Lands?</li>
+                <li class="adv-select__opt adv-select__opt--left_for_mission" data-field="left_for_mission">Left for Mission Lands</li>
                 <!--<li class="adv-select__opt adv-select__opt--collection" data-field="collection">Collection</li>-->
             </ul>
         </div>
