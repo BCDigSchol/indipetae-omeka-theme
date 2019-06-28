9dcc6da0ae8ee7cda26fd3b806992fc65a9bac32
Anna Kijas <annakijas1@gmail.com>
2019-06-28 12:20:41 -0400

    Update help.php

--- Elasticsearch/search/partials/help.php
+++ Elasticsearch/search/partials/help.php
@@ -11,43 +11,30 @@
         </td>
         <td>
             Some possible fields you can search by include:
-            <i>title</i>,
-            <i>description</i>,
-            <i>collection</i>,
-            <i>exhibit</i>,
-            <i>itemtype</i>,
-            <i>resulttype</i>,
-            <i>featured</i>,
-            <i>tags</i>,
-            <i>created</i>,
-            <i>updated</i>.
+            <i>Sender</i>,
+            <i>Recipient</i>,
+            <i>Models/Saints/Missionaries</i>,
+            <i>Other names</i>,
+            <i>From</i>,
+            <i>To</i>,
+            <i>Destination(s)</i>,
+            <i>Year</i>,
+            <i>Grade S.J.</i>, and
+            <i>Language of the Letter</i>.
             <br><br>
             Examples:
             <br>
-            <code>title:"Inhabited Spaces"</code><br>
-            <code>collection:Map*</code><br>
-            <code>itemtype:("Historical Map" OR "Still Image")</code><br>
-            <code>resulttype:Exhibit</code><br>
-            <code>featured:true</code><br>
-            <code>tags:forts</code><br>
-            <code>created:[2017-10-07 TO 2017-10-14]</code>
-            <code>updated:>=2017-11-01</code><br>
+            <code>sender:"Luciani, Tommaso"</code><br>
+            <code>recipient:Roothaan</code><br>
+            <code>from:Salerno</code><br>
+            <code>other names:"Antonio Ravalli"</code><br>
         </td>
     </tr>
     <tr>
         <td>Search using boolean operators and wildcards (defaults to OR):</td>
         <td>
-            <code>paris AND fortifications</code><br>
-            <code>title:paris AND itemType:Text</code><br>
-            <code>featured:true</code><br>
-            <code>184?s OR 185?s</code><br>
-            <code>updated:[2017-12-01 TO *] AND resulttype:exhibit</code>
-        </td>
-    </tr>
-    <tr>
-        <td>Boost searches:</td>
-        <td>Use the boost operator ^ to make one term more relevant than another. For example if we wanted to boost the term "paris" (the default boost is 1):<br><br>
-            <code>paris^2 western</code><br>
+            <code>Basile AND Roothaan</code><br>
+            <code>Scholastic OR Novice</code><br>
         </td>
     </tr>
 </table>
