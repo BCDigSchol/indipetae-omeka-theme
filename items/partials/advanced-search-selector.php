48a84a0b0deaf27514d085442265b2b66cc34c3e
Anna Kijas <annakijas1@gmail.com>
2019-08-08 16:52:20 -0400

    modified search options

--- items/partials/advanced-search-selector.php
+++ items/partials/advanced-search-selector.php
@@ -9,6 +9,7 @@
                 <li class="adv-select__opt adv-select__opt--recipient" data-field="recipient">Recipient</li>
                 <li class="adv-select__opt adv-select__opt--model" data-field="jmodel">Models/Saints/Missionaries</li>
                 <li class="adv-select__opt adv-select__opt--other_names" data-field="other_name">Other names</li>
+                <li class="adv-select__opt adv-select__opt--other_names" data-field="contributor">Transcriber</li>
             </ul>
         </div>
     </div>
@@ -36,7 +37,9 @@
         </button>
         <div class="dropdown-menu adv-select__opts">
             <ul>
-                <li class="adv-select__opt adv-select__opt--date" data-field="date">Year</li>
+                <li class="adv-select__opt adv-select__opt--date" data-field="year">Year</li>
+                <li class="adv-select__opt adv-select__opt--date" data-field="month">Month</li>
+                <li class="adv-select__opt adv-select__opt--date" data-field="day">Day</li>
             </ul>
         </div>
     </div>
@@ -52,7 +55,23 @@
                 <li class="adv-select__opt adv-select__opt--grade" data-field="grade">Grade S.J.</li>
                 <li class="adv-select__opt adv-select__opt--anterior_desire" data-field="anterior_desire">Anterior Desire</li>
                 <li class="adv-select__opt adv-select__opt--left_for_mission" data-field="left_for_mission">Left for Mission Lands</li>
+                <li class="adv-select__opt adv-select__opt--grade" data-field="language">Language</li>
                 <!--<li class="adv-select__opt adv-select__opt--collection" data-field="collection">Collection</li>-->
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-12">
+    <div class="btn-group dropright">
+        <button type="button" class="adv-select__other-btn btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Archive
+        </button>
+        <div class="dropdown-menu adv-select__opts">
+            <ul>
+                <li class="adv-select__opt adv-select__opt--grade" data-field="call_number">Archive</li>
+                <li class="adv-select__opt adv-select__opt--anterior_desire" data-field="folder">Folder</li>
+                <li class="adv-select__opt adv-select__opt--left_for_mission" data-field="number">Number</li>
             </ul>
         </div>
     </div>
