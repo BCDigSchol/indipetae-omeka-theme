d25182551588f4b339463e9be41842e9c60e688e
Benjamin Florin <benjamin.florin@gmail.com>
2020-02-05 12:12:40 -0500

    Update look & feel to match IAJS standards

--- common/partials/css.php
+++ common/partials/css.php
@@ -1,5 +1,5 @@
 <?php use BCLib\Indipetae\ThemeHelpers; ?>
-<link href="//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" media="all" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,600,600i,700,700i&display=swap" rel="stylesheet">
 <link href="/application/views/scripts/css/iconfonts.css?v=2.6.1" media="all" rel="stylesheet" type="text/css">
 <link href="<?= ThemeHelpers::CSS_PATH ?>/bcjs.css?v=2.6.1" media="all" rel="stylesheet" type="text/css">
 <link href="<?= ThemeHelpers::CSS_PATH ?>/bootstrap.min.css?v=2.6.1" media="all" rel="stylesheet" type="text/css">
