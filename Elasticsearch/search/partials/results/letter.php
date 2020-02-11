a07d313f4ac4446d7d99347058e5f7c0a4a983a2
Benjamin Florin <benjamin.florin@gmail.com>
2020-02-11 14:43:39 -0500

    Better individual result styling

--- Elasticsearch/search/partials/results/letter.php
+++ Elasticsearch/search/partials/results/letter.php
@@ -20,13 +20,19 @@ $recipients_heading = $recipients && count($elements['audience']) > 1 ? 'Recipie
 $destinations_heading = $destinations && count($elements['publisher']) > 1 ? 'Destinations' : 'Destination';
 
 ?>
-<ul>
+<ul class="elasticsearch-result__metadata-list">
     <?php if ($recipients): ?>
-        <li title="recipient"><b><?= $recipients_heading ?>:</b> <?= $recipients ?></li>
+        <li title="recipient" class="elasticsearch-result__metadata-row">
+            <span class="elasticsearch-result__metadata-header"><?= $recipients_heading ?>:</span>
+            <?= $recipients ?>
+        </li>
     <?php endif; ?>
 
-    <?php if ($destinations): ?>
-        <li title="destination"><b><?= $destinations_heading ?>:</b> <?= $destinations ?></li>
+    <?php if ($destinations && count($destinations)): ?>
+        <li title="destination" class="elasticsearch-result__metadata-row">
+            <span class="elasticsearch-result__metadata-header"><?= $destinations_heading ?>:</span>
+            <?= $destinations ?>
+        </li>
     <?php endif; ?>
 </ul>
 
